d8b2babd9bc11f98dccc7d4a62197d3b6f7591d6
l0nik <l0nik@mail.ru>
2024-12-23 19:17:37 +0300

    webpack + gitignore

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import '../pages/index.css';
+import { initialCards } from './cards.js';
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placesListElement = document.querySelector('.places__list');
+
+function createCard(cardData, deleteCardCallback) {
+  
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitleElement = cardElement.querySelector('.card__title');
+  const cardImageElement = cardElement.querySelector('.card__image');
+  const cardDeleteButtonElement = cardElement.querySelector('.card__delete-button');
+
+  cardTitleElement.textContent = cardData.name;
+  cardImageElement.src = cardData.link;
+  cardImageElement.alt = `${cardData.name} (фото)`;
+
+  cardDeleteButtonElement.addEventListener('click', deleteCardCallback);
+
+  return cardElement;
+
+}
+
+function deleteCard(event) {
+  const cardElement = event.target.closest('.places__item');
+  cardElement.remove();
+}
+
+for (const cardData of initialCards){
+  placesListElement.append(createCard(cardData, deleteCard));
+}
